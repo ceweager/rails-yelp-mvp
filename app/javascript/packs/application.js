564c6ea6cdc6204b18962bc341b5b2349c4354a9
Charlotte Weager <senwyn@googlemail.com>
2021-04-24 10:58:00 +0100

    added relevant validations to my models

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,10 +3,21 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("channels")
+// require("@rails/ujs").start()
+// require("turbolinks").start()
+// require("channels")
+// const { environment } = require('@rails/webpacker')
+// const webpack = require('webpack')
+// environment.plugins.prepend('Provide',
+//   new webpack.ProvidePlugin({
+//     $: 'jquery',
+//     jQuery: 'jquery',
+//     Popper: ['popper.js', 'default']
+//   })
+// )
 
+// module.exports = environment
+// import 'bootstrap';
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
